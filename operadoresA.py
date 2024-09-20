e85821caafa78e84fe4bac149e554f4a3596f725
Ailin-Miscles-5J <a.22308051281264@cbtis128.edu.mx>
2024-09-20 09:27:19 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,46 @@
+print("Act 9 Clases V2")
+print("Ailin Ruvalcaba Mat: 22308051281264")
+# Zona de class
+class Operadores1264:
+    # Zona de funciones
+    def suma1264(self,A,R):
+        s1264=A+R
+        print(f"La suma de {A} + {R} = {s1264}")
+    def resta1264(self,A,R):
+        s1264=A-R
+        print(f"La resta de {A} - {R} = {s1264}")
+    def multi1264(self,A,R):
+        s1264=A*R
+        print(f"La multiplicacion de {A} * {R} = {s1264}")
+    def div1264(self,A,R):
+        s1264=A/R
+        print(f"La division de {A} / {R} = {s1264}")
+    def mod1264(self,A,R):
+        s1264=A%R
+        print(f"El modulo de {A} % {R} = {s1264}")
+    def expo1264(self,A,R):
+        s1264=A**R
+        print(f"El exponente de {A} ** {R} = {s1264}")
+    def coci1264(self,A,R):
+        s1264=A//R
+        print(f"El cociente de {A} // {R} = {s1264}")
+
+
+# Zona de creacion del Objeto
+operailin=Operadores1264()
+
+# Zona de uso de objetos
+print(" Funcion Suma")
+operailin.suma1264(5,5)
+print(" Funcion Resta")
+operailin.resta1264(10,5)
+print(" Funcion Multiplicacion")
+operailin.multi1264(5,2)
+print(" Funcion Division")
+operailin.div1264(50,4)
+print(" Funcion Modulo")
+operailin.mod1264(10,2)
+print(" Funcion Exponente")
+operailin.expo1264(3,2)
+print(" Funcion Cociente")
+operailin.coci1264(20,4)
